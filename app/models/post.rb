1d6f336b7ac75bf2cd92810be7e393133494461d
sco-bo <scottebobbitt@gmail.com>
2016-02-09 14:02:48 -0500

    validates presence/length of title and presence of user_id

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true, length: {minimum: 1, maximum: 300}
+  validates :user, presence: true
+
   belongs_to :user
   has_many :comments, dependent: :destroy
 end
