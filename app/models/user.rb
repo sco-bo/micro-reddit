c4cd14641545d7f04ab56d74d37930dfc6f8fb2a
sco-bo <scottebobbitt@gmail.com>
2016-02-09 13:28:22 -0500

    validates presence and uniqueness of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, length: {maximum: 20}, 
+                   uniqueness: {case_sensitive: false}
+                   
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 end
